f7fe059133ad08f048461f25bf99f02e3b8fe518
Ayc0 <ayc0.benj@gmail.com>
2022-11-01 21:04:58 +0000

    update all deps for next example

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,5 +1,22 @@
 const Index = () => {
-  return matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+  return (
+    <>{matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"}</>
+  );
 };
+
+export async function getServerSideProps() {
+  // To be honest, it doesn’t really makes sense to use `mock-match-media` for server side rendering.
+  // Even if you mock it, the value will never match your end users.
+  // Instead, you should just check in the component if you are on the client side / server side.
+  require("mock-match-media/polyfill");
+  const { setMedia } = require("mock-match-media");
+  setMedia({
+    "prefers-color-scheme": "dark",
+  });
+
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
 
 export default Index;
